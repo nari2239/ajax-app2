0ffeb430a78065b638e37b8782bc486bc7792546
nari2239 <oryolife@gmail.com>
2021-01-30 20:26:07 +0900

    各投稿をクリックした時に発火する関数を記述

---
+++ app/javascript/checked.js
@@ -0,0 +1,10 @@
+function check() {
+  const posts = document.querySelectorAll(".post");
+  posts.forEach(function (post) {
+    post.addEventListener("click", () => {
+
+    });
+  });
+}
+
+window.addEventListener("load", check);
