7867d7bd8db20b79d0dfe7e24b128a95c59f1ea3
nari2239 <oryolife@gmail.com>
2021-02-09 16:40:29 +0900

    どのようなリクエストにするかを記述

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,6 +2,7 @@ function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
   });
 }
 
