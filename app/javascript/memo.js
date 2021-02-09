f297aa1e6845b17f6ceb145a6f8f019ab1b04a8d
nari2239 <oryolife@gmail.com>
2021-02-09 16:43:48 +0900

    フォームの値を取得し、送信

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,9 +1,12 @@
 function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    // new FormData(フォームの要素);でフォームに入力された値を取得することができる
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
+    XHR.send(formData);
   });
 }
 
